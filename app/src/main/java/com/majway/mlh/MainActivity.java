d54306cf8b888e48de1ab97376506c21be969d09
Rob <rmay@ghx.com>
2017-01-10 08:26:49 -0700

    Simple GUI

--- app/src/main/java/com/majway/mlh/MainActivity.java
+++ app/src/main/java/com/majway/mlh/MainActivity.java
@@ -1,7 +1,11 @@
 package com.majway.mlh;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
