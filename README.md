ff7f6a81dba6a39e0ce19553eb9d4e6cbb79a04a
Rob <majway27.1@gmail.com>
2017-01-15 07:01:14 -0700

    Basic Post inside of AsyncTask.

--- README.md
+++ README.md
@@ -2,16 +2,18 @@
 Mother's Little Helper 2, Android App
 
 ## ToDo
-* Simple GUI
-  * Schedule request now
-  * Schedule pending request
-  * View pending requests
+* ~~Simple GUI~~
+  * ~~Schedule request now~~
+  * ~~Schedule pending request~~
+  * ~~View pending requests~~
 * Simple post with known static values to create apt
+  * AsyncTask for call
 * Save pending request, scheduler picks up and posts
 * Scheduler understands 2 day wait and monitors and schedules valid targets from list
 * Store Credentials safely
 * Fancy GUI
 * Assets pickable from pick list
+* MPL ?
 
 ## Considering
 * Push notifications when activity happens?
@@ -20,3 +22,10 @@ Mother's Little Helper 2, Android App
 ## Notes
 * Requires API 21, min version 5
 * Android Studio
+
+## Mechanize
+* http://stackoverflow.com/questions/16608135/android-studio-add-jar-as-library
+* https://github.com/GistLabs/mechanize
+* http://search.maven.org/#artifactdetails%7Ccom.gistlabs%7Cmechanize%7C2.0.0-RC1%7Cjar
+
+
--- app/src/main/java/com/majway/mlh/NewAppointment.java
+++ app/src/main/java/com/majway/mlh/NewAppointment.java
@@ -1,12 +1,27 @@
 package com.majway.mlh;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.view.View;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
+
+import com.gistlabs.mechanize.Mechanize;
+import com.gistlabs.mechanize.document.AbstractDocument;
+import com.gistlabs.mechanize.document.html.HtmlDocument;
+import com.gistlabs.mechanize.impl.MechanizeAgent;
+import com.gistlabs.mechanize.interfaces.Resource;
+import com.gistlabs.mechanize.parameters.Parameters;
+import com.gistlabs.mechanize.interfaces.document.Document;
+
+
 
 public class NewAppointment extends AppCompatActivity {
+
+    private TextView returnStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +29,83 @@ public class NewAppointment extends AppCompatActivity {
         setContentView(R.layout.activity_new_appointment);
 
         Intent intent = getIntent();
-        String message = "test";
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
-
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_new_appointment);
-        layout.addView(textView);
+        String output="Get Ready!";
+        returnStatus=(TextView)findViewById(R.id.textView1);
+        returnStatus.setText(output);
     }
-}
+
+    public String sendRequest()
+    {
+        AsyncTaskRunner runner = new AsyncTaskRunner();
+        runner.execute();
+        String output = "google title";
+        return output;
+    }
+
+    public void onBtnClicked(View v)
+    {
+        if(v.getId() == R.id.btnSendAppointmentRequest)
+        {
+            String output = sendRequest();
+            // String output="Output from Call";
+            TextView returnStatus = (TextView)findViewById(R.id.textView1);
+            returnStatus.setText(output);
+            MessageBox("Request Sent");
+        }
+    }
+
+    public void MessageBox(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+
+        private String resp;
+        ProgressDialog progressDialog;
+
+        @Override
+        protected String doInBackground(String... params) {
+            onProgressUpdate("Scheduling Appointment.."); // Calls onProgressUpdate()
+            try {
+                MechanizeAgent agent = new MechanizeAgent();
+                Parameters parameters = new Parameters().add("param1", "value").add("param2", "value2");
+                HtmlDocument page = agent.post("http://posttestserver.com/post.php", parameters);
+                String pageString = page.htmlElements().toString();
+                if (!(pageString.contains(" Successfully dumped 2 post variables"))) {
+                    throw new Exception("Appointment Request Call Failed!");
+                }
+                System.out.println(pageString);
+                resp = pageString;
+                //resp = "test";
+                return  resp;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            } catch (Exception e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            }
+            return resp;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // execution of result of Long time consuming operation
+            progressDialog.dismiss();
+            returnStatus.setText(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(NewAppointment.this,
+                    "ProgressDialog",
+                    "Status: Starting Call");
+        }
+
+        protected void onProgressUpdate(String result) {
+            progressDialog.setMessage("Status: " + result);
+        }
+    }
+
+ }
